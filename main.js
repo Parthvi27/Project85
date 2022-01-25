e1cad51105e1d4946eb825121f66f453273fa1d2
Parthvi27 <85880172+Parthvi27@users.noreply.github.com>
2022-01-25 22:00:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-//Create a reference for canvas 
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d"); 
 
-//Give specific height and width to the car image
+
 
 background_image = "parkingLot.jpg";
 greencar_image = "car2.png";
 
-//Set initial position for a car image.
+greencar_width = 75;
+greencar_height = 100;
+
+greencar_x = 5;
+greencar_y = 225;
+
 
 function add() {
-	//upload car, and background images on the canvas.
+	background_imageTag = new Image();
+	background_imageTag.onload = uploadBackground;
+	background_imageTag.src = background_image;
+
+	greencar_imageTag = new Image();
+	greencar_imageTag.onload = uploadBackground;
+	greencar_imageTag.src = greencar_image
 }
 
 function uploadBackground() {
-	//Define function ‘uploadBackground’
+	ctx.drawImage(background_imageTag,0,0,canvas.width,canvas.height);
 
 }
 
 function uploadgreencar() {
-	//Define function ‘uploadgreencar’.
-
+	ctx.drawImage(greencar_imageTag,greencar_x,greencar_y,greencar_width,greencar_height);
 	
 }
 
@@ -58,20 +69,40 @@ function my_keydown(e)
 
 function up()
 {
-	//Define function to move the car upward
+	if(greencar_y >=0){
+		greencar_y=greencar_y - 10;
+		console.log("when up arrow is pressed, x ="+greencar_x+"| y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
 
 function down()
 {
-	//Define function to move the car downward
+	if(greencar_y <=300){
+		greencar_y=greencar_y + 10;
+		console.log("when down arrow is pressed, x ="+greencar_x+"| y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
 
 function left()
 {
-	//Define function to move the car left side
-}
+	if(greencar_x >=0){
+		greencar_x=greencar_x - 10;
+		console.log("when left arrow is pressed, x ="+greencar_x+"| y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 
+}
 function right()
 {
-	//Define function to move the car right side
+	if(greencar_x <=700){
+		greencar_x=greencar_x + 10;
+		console.log("when right arrow is pressed, x ="+greencar_x+"| y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
